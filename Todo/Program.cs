edd34b613fd73bc4679600e99498298c45add0f9
景崴 許 <peter.hsu0825@outlook.com>
2022-03-22 01:38:01 +0800

    建立Model與Scaffolded Controller

--- Todo/Program.cs
+++ Todo/Program.cs
@@ -1,4 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Todo.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TodoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
